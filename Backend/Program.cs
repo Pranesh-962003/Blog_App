e5c05e46ed71092dd586a6450fb477e74d92e324
Pranesh-962003 <pranesh962003.p@gmail.com>
2025-06-18 21:22:09 +0530

    Update Program.cs

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -34,17 +34,11 @@ builder.Services.AddSignalR();
 
 
 
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
 
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
 app.UseHttpsRedirection();
 
